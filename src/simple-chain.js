c7e839972ca6db899488e315f9da2f1438dab7a7
Maxim Zorin <maxim.zorin@orioninc.com>
2023-04-16 02:35:50 +0300

    feat: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+  delete() {
+    this.chain.length = 0
+  },
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(value)
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    position = position - 1
+    if (typeof position !== 'number' || !Number.isInteger(position) || !(0 <= position && position < this.chain.length)) {
+      this.delete()
+      throw new Error('You can\'t remove incorrect link!')
+    }
+    this.chain.splice(position, 1)
+    return this
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const value = this.chain.map(x => `( ${x} )`).join('~~')
+    this.delete()
+    return value
   }
 };
 
